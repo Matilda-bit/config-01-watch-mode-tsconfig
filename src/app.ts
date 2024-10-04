cb48721943d6c8aab9c0d0dfaac861750822dc6f
Matilda-bit <polinaovras@gmail.com>
2024-10-04 19:38:04 +0300

    singeleton & private constructor

--- src/app.ts
+++ src/app.ts
@@ -48,6 +48,7 @@ abstract class Department {
   
   class AccountingDepartment extends Department {
     private lastReport: string;
+    private static instance: AccountingDepartment;
   
     get mostRecentReport() {
       if (this.lastReport) {
@@ -63,15 +64,23 @@ abstract class Department {
       this.addReport(value);
     }
   
-    constructor(id: string, private reports: string[]) {
+    private constructor(id: string, private reports: string[]) {
       super(id, 'Accounting');
       this.lastReport = reports[0];
+    }
+
+    static getInstance() {
+        if(AccountingDepartment.instance) {
+            return this.instance;
+        } else {
+            this.instance = new AccountingDepartment('d2', []);
+            return this.instance;
+        }
     }
 
     describe() {
         console.log('Accounting Department - ID: ' + this.id);
     }
-
   
     addEmployee(name: string) {
       if (name === 'Polina') {
@@ -106,7 +115,11 @@ abstract class Department {
   
   console.log(it);
   
-  const accounting = new AccountingDepartment('d2', []);
+  //   const accounting = new AccountingDepartment('d2', []);
+  const accounting = AccountingDepartment.getInstance();
+  const accounting2 = AccountingDepartment.getInstance();
+
+  console.log(accounting, accounting2);
   
   accounting.mostRecentReport = 'Year End Report';
   accounting.addReport('Something went wrong...');
